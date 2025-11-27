e08d878b3f5e244332fe0b5b9c4f0202d4a3ab79
Al Amin Khan <alaminkhan2010@gmail.com>
2025-11-27 17:27:33 +0600

    fix: auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,16 +1,19 @@
-// auth.ts
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
 
-export const {
-    handlers: { GET, POST },
-    auth,
-    signIn,
-    signOut,
-} = NextAuth({
+export const authOptions = {
     adapter: PrismaAdapter(db),
     session: { strategy: "database" },
     ...authConfig,
-});
+};
+
+const handler = NextAuth(authOptions);
+
+export const GET = handler;
+export const POST = handler;
+
+export async function auth() {
+    return getServerSession(authOptions);
+}
